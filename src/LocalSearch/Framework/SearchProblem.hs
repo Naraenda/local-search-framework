f8f949e78e3a2dc27dd482e08163bc159d5a007c
Naraenda <naraenda@prasetya.eu>
2020-03-27 15:02:10 +0100

    Simple problem composition

--- src/LocalSearch/Framework/SearchProblem.hs
+++ src/LocalSearch/Framework/SearchProblem.hs
@@ -1,6 +1,7 @@
 module LocalSearch.Framework.SearchProblem where
 
 import Control.Monad.Random.Lazy
+import           Data.Maybe                     ( maybeToList )
 
 type Score = Float
 
@@ -17,3 +18,28 @@ class Searchable a where
   -- | Returns a random nieghbouring state
   randomNeighbour :: RandomGen g => a -> Rand g (Maybe a)
   randomNeighbour = uniformMay . neighbours
+
+-- | Combines two searchable problems intro one
+data Composed a b = Composed (a -> b -> Score) a b
+
+instance (Searchable a, Searchable b) => Searchable (Composed a b) where
+  score (Composed f a b) = f a b
+
+  neighbours (Composed f a b) = 
+    [Composed f a' b | a' <- neighbours a] ++
+    [Composed f a b' | b' <- neighbours b]
+
+  randomNeighbour (Composed f a b) = do
+    a' <- randomNeighbour a
+    b' <- randomNeighbour b
+    let ca = flip (Composed f) b <$> maybeToList a'
+    let cb = Composed f a <$> maybeToList b'
+    uniformMay (ca ++ cb)
+
+-- | Sums the scores of two search problems
+sumScores :: (Searchable a, Searchable b) => a -> b -> Score
+sumScores a b = score a + score b
+
+-- | Multiplies the scores of two search problems
+mulScores :: (Searchable a, Searchable b) => a -> b -> Score
+mulScores a b = score a * score b
