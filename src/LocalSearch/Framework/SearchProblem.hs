25fdd08ffa1345f5237f0be3ed3e85fbd692b116
Naraenda <naraenda@prasetya.eu>
2020-03-28 16:01:05 +0100

    Made exploration and action seperate in Searchproblem.

--- src/LocalSearch/Framework/SearchProblem.hs
+++ src/LocalSearch/Framework/SearchProblem.hs
@@ -1,45 +1,41 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module LocalSearch.Framework.SearchProblem where
 
 import Control.Monad.Random.Lazy
-import           Data.Maybe                     ( maybeToList )
+import Data.Maybe (maybeToList)
 
 type Score = Float
 
 -- Maybe it's better to have a generic (class) definition of a search problem?
 -- It makes it implementing the solver and problem both more easily.
 
-class Searchable a where
+class Searchable a b | a -> b where
   -- | Gives the current state a score
   score :: a -> Score
 
   -- | Returns all neighbouring states
-  neighbours :: a -> [a]
+  neighbours :: a -> [b]
 
-  -- | Returns a random nieghbouring state
-  randomNeighbour :: RandomGen g => a -> Rand g (Maybe a)
-  randomNeighbour = uniformMay . neighbours
+  -- | Explore a neighbour using an action
+  explore :: a -> b -> a
 
 -- | Combines two searchable problems intro one
 data Composed a b = Composed (a -> b -> Score) a b
 
-instance (Searchable a, Searchable b) => Searchable (Composed a b) where
+instance (Searchable a fa, Searchable b fb) => Searchable (Composed a b) (Either fa fb) where
   score (Composed f a b) = f a b
 
-  neighbours (Composed f a b) = 
-    [Composed f a' b | a' <- neighbours a] ++
-    [Composed f a b' | b' <- neighbours b]
+  neighbours (Composed f a b) = (Left <$> neighbours a) ++ (Right <$> neighbours b)
 
-  randomNeighbour (Composed f a b) = do
-    a' <- randomNeighbour a
-    b' <- randomNeighbour b
-    let ca = flip (Composed f) b <$> maybeToList a'
-    let cb = Composed f a <$> maybeToList b'
-    uniformMay (ca ++ cb)
+  explore (Composed f a b) (Left  fa) = Composed f (explore a fa) b
+  explore (Composed f a b) (Right fb) = Composed f a (explore b fb)
 
 -- | Sums the scores of two search problems
-sumScores :: (Searchable a, Searchable b) => a -> b -> Score
+sumScores :: (Searchable a fa, Searchable b fb) => a -> b -> Score
 sumScores a b = score a + score b
 
 -- | Multiplies the scores of two search problems
-mulScores :: (Searchable a, Searchable b) => a -> b -> Score
+mulScores :: (Searchable a fa, Searchable b fb) => a -> b -> Score
 mulScores a b = score a * score b
