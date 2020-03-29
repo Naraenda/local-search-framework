97a2805da6a2051b1fc1b9449a9157ea53459922
Bart Wijgers <bartwijgers@hotmail.com>
2020-03-29 11:29:00 +0200

    Add state to GA parameters
    Turns out we need something defining the state space in the genetic
    algorithms to make valid individuals at the start, and to generate
    proper crossing over and mutation values. Therefore, we add this as a
    parameter to the typeclasses and a lot of functions.

--- src/LocalSearch/Framework/GeneticAlgorithm.hs
+++ src/LocalSearch/Framework/GeneticAlgorithm.hs
@@ -2,7 +2,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 module LocalSearch.Framework.GeneticAlgorithm
-  (
+  ( EnumRandom(..)
+  , GeneticAlgorithm(..)
+
+  , Fitness
+  , Iterations
+  , PopulationSize
+
+  , runGenetic
   )
 where
 
@@ -15,23 +22,23 @@ type Fitness = Double
 type Iterations = Int
 type PopulationSize = Int
 
-class EnumRandom a where
-  getRandomValue :: RandomGen g => Rand g a
+class EnumRandom s a | a -> s where
+  getRandomValue :: RandomGen g => s -> Rand g a
 
 -- | Defines a genetic algorithm on type `a`. This requires two additional
 -- types, `cross`, defining what kinds of crossover can happen, and `mut`,
 -- which defines what kind of mutations can happen. The user needs to implement
 -- the functions crossover and mutation based on these types. The value passed
 -- to these functions will be generated automatically by the solver.
-class (EnumRandom cross, EnumRandom mut) =>
-      GeneticAlgorithm cross mut a | a -> mut, a -> cross where
-  randomIndividual  :: RandomGen g => Rand g a
+class (EnumRandom space cross, EnumRandom space mut) =>
+      GeneticAlgorithm cross mut space a | a -> mut, a -> cross, a -> space where
+  randomIndividual  :: RandomGen g => space -> Rand g a
   fitness           :: a -> Fitness
   crossover         :: cross -> a -> a -> a
   mutation          :: mut -> a -> a
 
-runGenetic :: GeneticAlgorithm cross mut a => IO a
-runGenetic = evalRandIO $ runGeneticRand popSize iters
+runGenetic :: GeneticAlgorithm cross mut space a => space -> IO a
+runGenetic space = evalRandIO $ runGeneticRand popSize iters space
   where
     popSize = 100
     iters   = 100
@@ -45,24 +52,26 @@ takeMax f = fmap fst . tm' Nothing f
       | v < f y   = tm' (Just (y, f y)) f ys
       | otherwise = tm' a               f ys
 
-runGeneticRand :: (GeneticAlgorithm cross mut a, RandomGen g)
+runGeneticRand :: (GeneticAlgorithm cross mut space a, RandomGen g)
                => Int
                -> Iterations
+               -> space
                -> Rand g a
-runGeneticRand popSize iters =
+runGeneticRand popSize iters space =
   do
-    pop       <- makeRandomPopulation popSize
-    finalPop  <- runGenetic' iters (popSize, pop)
+    pop       <- makeRandomPopulation space popSize
+    finalPop  <- runGenetic' space iters (popSize, pop)
     return $ selectBest finalPop
   where
     selectBest = fst . fromJust . takeMax snd . map (\x -> (x, fitness x)) . snd
 
-makeRandomPopulation :: (GeneticAlgorithm cross mut a, RandomGen g)
-                     => Int
+makeRandomPopulation :: (GeneticAlgorithm cross mut space a, RandomGen g)
+                     => space
+                     -> Int
                      -> Rand g [a]
-makeRandomPopulation = sequence . flip replicate randomIndividual
+makeRandomPopulation s = sequence . flip replicate (randomIndividual s)
 
-getParents :: (GeneticAlgorithm cross mut a, RandomGen g)
+getParents :: (GeneticAlgorithm cross mut space a, RandomGen g)
            => [a]
            -> Rand g (a, a)
 getParents pop = gp'
@@ -78,25 +87,27 @@ getParents pop = gp'
       | x <= w = y
       | otherwise = getParent (x - w) pop
 
-generateNewPopulation :: (GeneticAlgorithm cross mut a, RandomGen g)
-                      => PopulationSize
+generateNewPopulation :: (GeneticAlgorithm cross mut space a, RandomGen g)
+                      => space
+                      -> PopulationSize
                       -> [a]
                       -> Rand g [a]
-generateNewPopulation popSize pop = sequence . replicate popSize $
+generateNewPopulation space popSize pop = sequence . replicate popSize $
   do
     (p1, p2) <- getParents pop
-    co <- getRandomValue
+    co <- getRandomValue space
     let child = crossover co p1 p2
-    mut <- getRandomValue
+    mut <- getRandomValue space
     return $ mutation mut child
 
-runGenetic' :: (GeneticAlgorithm cross mut a, RandomGen g)
-            => Iterations
+runGenetic' :: (GeneticAlgorithm cross mut space a, RandomGen g)
+            => space
+            -> Iterations
             -> (PopulationSize, [a])
             -> Rand g (PopulationSize, [a])
-runGenetic' 0 x = return x
-runGenetic' n (popSize, pop) =
+runGenetic' _ 0 x = return x
+runGenetic' space n (popSize, pop) =
   do
-    newPop <- generateNewPopulation popSize pop
-    runGenetic' (n - 1) (popSize, newPop)
+    newPop <- generateNewPopulation space popSize pop
+    runGenetic' space (n - 1) (popSize, newPop)
 
